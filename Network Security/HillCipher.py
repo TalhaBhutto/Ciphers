5731d5ef79e574625d7b09ee344e9ca8923f60f1
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-01-10 18:43:29 +0500

    Update HillCipher.py

--- Network Security/HillCipher.py
+++ Network Security/HillCipher.py
@@ -1,35 +1,37 @@
 #Made By Talha Hussain  Bhutto B17158051
-import numpy
 def encrypt(data,key):
+    d=data
+    data=""
+    for x in d:
+        if(x!=" "):
+            data=data+x
     temp=[]
     a=0
     b=0
     if(len(data)%2==1):
         data=data+"X"
     stop=len(data)/2
-    l1=1
-    while(l1<stop+1):
+    l1=0
+    while(l1<stop):
         a=b=0
-        if(data[l1*2]>="a" and data[l1*2]<="z"):
+        if(data[l1*2]>="a" and data[l1*2]<="z" and data[l1*2+1]>="a" and data[l1*2+1]<="z"):
             a=ord(data[l1*2])-97
-        elif(data[l1*2]>="A" and data[l1*2]<="Z"):
+            b=ord(data[l1*2+1])-97
+        elif(data[l1*2]>="A" and data[l1*2]<="Z" and data[l1*2+1]>="A" and data[l1*2+1]<="Z"):
             a=ord(data[l1*2])-65
-        else:
-            return "Plese enter a character from A-Z or a-z"
-        if(data[l1*2-1]>="a" and data[l1*2-1]<="z"):
-            b=ord(data[l1*2-1])-97
-        elif(data[l1*2-1]>="A" and data[l1*2-1]<="Z"):
-            b=ord(data[l1*2-1])-65
+            b=ord(data[l1*2+1])-65
         else:
             return "Plese enter a character from A-Z or a-z"
         temp.append([a,b])
+        l1+=1
     return temp   
 def decrypt(a,b):
     a+b
 def Encryption():
     data=input("Enter a message for encryption: ")
     A=input("Enter the key : ")
-    print("The encrypted message is:\n"+encrypt(data,A))
+    #print("The encrypted message is:\n"+encrypt(data,A))
+    print(encrypt(data,A))
 def Decryption():
     data=input("Enter a message for decryption: ")
     A=input("Enter the key : ")
